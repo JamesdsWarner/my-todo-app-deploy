56c0fac602019ce3f9dcbac03c0bcd4170cf3601
JamesdsWarner <jamesdswarner@gmail.com>
2023-01-05 01:12:35 +0000

    backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,11 @@
-import bcryptjs from "bcryptjs";
-import createError from "../utils/createError.js";
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import bcryptjs from 'bcryptjs';
+import createError from '../utils/createError.js';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
 
 export const register = async (req, res, next) => {
   if (!req.body.name || !req.body.email || !req.body.password) {
-    return next(createError({ status: 400, message: "Name, email, password is required" }));
+    return next(createError({ status: 400, message: 'Name, email, password is required' }));
   }
 
   try {
@@ -18,7 +18,7 @@ export const register = async (req, res, next) => {
       password: hashedPassword,
     });
     await newUser.save();
-    return res.status(201).json("New user created");
+    return res.status(201).json('New user created');
   } catch (err) {
     console.log(err);
     return next(err);
@@ -27,30 +27,30 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
   if (!req.body.email || !req.body.password) {
-    return next(createError({ status: 400, message: "Name, email, password is required" }));
+    return next(createError({ status: 400, message: 'Name, email, password is required' }));
   }
   try {
-    const user = await User.findOne({ email: req.body.email }).select("name email password");
+    const user = await User.findOne({ email: req.body.email }).select('name email password');
     if (!user) {
-      return next(createError({ status: 404, message: "No user found" }));
+      return next(createError({ status: 404, message: 'No user found' }));
     }
     const isPasswordCorrect = await bcryptjs.compare(req.body.password, user.password);
     if (!isPasswordCorrect) {
-      return next(createError({ status: 400, message: "Password is incorrect" }));
+      return next(createError({ status: 400, message: 'Password is incorrect' }));
     }
     const payload = {
       id: user._id,
       name: user.name,
     };
     const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: "1d",
+      expiresIn: '1d',
     });
     return res
-      .cookie("access_token", token, {
+      .cookie('access_token', token, {
         httpOnly: true,
       })
       .status(200)
-      .json({ message: "login success" });
+      .json({ message: 'login success' });
   } catch (err) {
     console.log(err);
     return next(err);
@@ -58,19 +58,19 @@ export const login = async (req, res, next) => {
 };
 
 export const logout = (req, res) => {
-  res.clearCookie("access_token");
-  return res.status(200).json({ message: "Logout success" });
+  res.clearCookie('access_token');
+  return res.status(200).json({ message: 'Logout success' });
 };
 
 export const isLoggedIn = (req, res) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return res.json(false);
+    return res.json(true);
   }
   return jwt.verify(token, process.env.JWT_SECRET, (err) => {
     if (err) {
       return res.json(false);
     }
-    return res.json(true);
+    res.redirect('/');
   });
 };
--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-import { MongoClient } from "mongodb";
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-import mongoose from "mongoose";
-import "dotenv/config";
-import allRoutes from "./routes/index.js";
+import { MongoClient } from 'mongodb';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+import allRoutes from './routes/index.js';
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
 // middleware
 app.use(cors());
-app.use(morgan("tiny"));
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser());
 
 // routes
-app.use("/api", allRoutes);
+app.use('/api', allRoutes);
 
 // error handler
 app.use((err, req, res, next) => {
   const status = err.status || 500;
-  const message = err.message || "Internal Server Error";
+  const message = err.message || 'Internal Server Error';
 
   return res.status(status).json({ message, stack: err.stack });
 });
@@ -30,9 +31,9 @@ app.use((err, req, res, next) => {
 const connectDB = async () => {
   const connectionString = process.env.ATLAS_URI;
   try {
-    mongoose.set("strictQuery", false);
+    mongoose.set('strictQuery', false);
     await mongoose.connect(connectionString);
-    console.log("mongodb connected");
+    console.log('mongodb connected');
   } catch (e) {
     console.error(e);
     process.exit(1);
