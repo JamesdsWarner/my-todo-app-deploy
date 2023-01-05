40880aaa750d44065e3753b2e4a97315f8f978e1
JamesdsWarner <jamesdswarner@gmail.com>
2023-01-05 21:28:29 +0000

    backend

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'POST');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
   next();
 });
-app.use(cors());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser());
