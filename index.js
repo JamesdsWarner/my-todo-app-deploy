0fda3faa24987cce2fc8986ae409f934e6009d85
JamesdsWarner <jamesdswarner@gmail.com>
2023-01-05 19:16:57 +0000

    backend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://todo-back.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', true);
 
