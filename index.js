bd7ab367696c3792aee198cbf3ceed54e221089d
JamesdsWarner <jamesdswarner@gmail.com>
2023-01-05 21:18:13 +0000

    backend

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const app = express();
 // middleware
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'POST');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', true);
 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
   next();
