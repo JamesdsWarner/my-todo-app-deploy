21a6522c848104704bd75fe2433b28d8bfed5892
JamesdsWarner <jamesdswarner@gmail.com>
2023-01-05 19:34:46 +0000

    backend

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ const app = express();
 
 // middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/auth');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  'Access-Control-Allow-Methods', 'POST';
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', true);
 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
   next();
 });
-app.use(cors({ credentials: true, origin: 'http://localhost:3000/auth' }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser());
