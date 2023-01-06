f6d76e9a25dbf2f8f4f8e53db7be1c9a97b0803d
JamesdsWarner <jamesdswarner@gmail.com>
2023-01-06 00:08:36 +0000

    backend

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://bejewelled-zuccutto-dfa6b6.netlify.app/');
   res.header('Access-Control-Allow-Methods', 'POST');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', true);
