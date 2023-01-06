6a4200c96d0b7c89a91001cf618f0d3522aa16ea
JamesdsWarner <jamesdswarner@gmail.com>
2023-01-06 12:44:49 +0000

    changing index file

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 app.use(morgan('tiny'));
 app.use(cookieParser());
 
-const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://shrouded-journey-38552.heroku...'];
+const whitelist = ['http://localhost:3000', 'http://localhost:8080', 'https://my-todo-deploy.herokuapp.com/'];
 const corsOptions = {
   origin(origin, callback) {
     console.log(`** Origin of request ${origin}`);
