5693215745549a5f04f66866b94243f2709f285a
JamesdsWarner <jamesdswarner@gmail.com>
2023-01-06 12:52:00 +0000

    push to heroku

--- index.js
+++ index.js
@@ -52,10 +52,10 @@ app.use((err, req, res, next) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
+  app.use(express.static(path.join('backend', 'frontend/build')));
   // Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    res.sendFile(path.join('backend', 'frontend/build', 'index.html'));
   });
 }
 
